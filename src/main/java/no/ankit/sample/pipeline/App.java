ca7d337628e823d4991428b95a4bf6544d08e4c4
Ankit <ankit.java8@gmail.com>
2017-08-05 14:13:17 +0200

    initial

---
+++ src/main/java/no/ankit/sample/pipeline/App.java
@@ -0,0 +1,25 @@
+package no.ankit.sample.pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+/**
+ * Created by Ankit on 03-08-2017.
+ */
+@SpringBootApplication
+@RestController
+public class App {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(App.class, args);
+    }
+}
