9f4a611031f34cab499b737f0c55f6d1947c13a9
Ankit <ankit.java8@gmail.com>
2017-08-06 00:16:20 +0200

    new branch changes

--- src/main/java/no/ankit/sample/pipeline/App.java
+++ src/main/java/no/ankit/sample/pipeline/App.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-
 /**
  * Created by Ankit on 03-08-2017.
  */
@@ -16,6 +14,7 @@ public class App {
 
     @RequestMapping("/")
     public String home() {
+        System.out.println("new branch change");
         return "Hello World!";
     }
 
