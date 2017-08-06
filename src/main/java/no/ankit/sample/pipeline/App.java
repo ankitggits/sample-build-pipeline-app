d961873906b4f2dee9bd3edac63eae9197065f31
Ankit <ankit.java8@gmail.com>
2017-08-06 02:52:24 +0200

    latest

--- src/main/java/no/ankit/sample/pipeline/App.java
+++ src/main/java/no/ankit/sample/pipeline/App.java
@@ -2,6 +2,8 @@ package no.ankit.sample.pipeline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +14,11 @@ import java.util.Arrays;
  */
 @SpringBootApplication
 @RestController
-public class App {
+public class App extends SpringBootServletInitializer {
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application;
+    }
 
     @RequestMapping("/")
     public String home() {
